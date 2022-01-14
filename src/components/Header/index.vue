09fcc650d28067abb5ba874c866b5501dd57f17c
gientech <gxh__2020@163.com>
2022-01-15 00:07:20 +0800

    初始化项目

---
+++ src/components/Header/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<!-- 头部第一行 -->
+		<div class="top">
+			<div class="container">
+				<div class="loginList">
+					<p>尚品汇欢迎您</p>
+					<p>
+						<span>请</span>
+						<a href="###">登录</a>
+						<a href="###" class="register">免费注册</a>
+					</p>
+				</div>
+				<div class="typeList">
+					<a href="###">我的订单</a>
+					<a href="###">我的购物车</a>
+					<a href="###">我的尚品汇</a>
+					<a href="###">尚品汇会员</a>
+					<a href="###">企业采购</a>
+					<a href="###">关注尚品汇</a>
+					<a href="###">合作招商</a>
+					<a href="###">商家后台</a>
+				</div>
+			</div>
+		</div>
+		<!-- 头部第二行  搜索区域 -->
+		<div class="bottom">
+			<h1 class="logoArea">
+				<a href="###" class="logo" title="尚品汇" target="_blank">
+					<img src="../../assets/logo.png" alt="" >
+				</a>
+			</h1>
+			<div class="searchArea">
+				<form action="###" class="searchForm">
+					<input type="text" id="autocomplete" class="input-error input-xxlarge" value="" />
+					<button class="sui-btn btn-xlarge btn-danger" type="button">搜索</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+</template>
+
+<script>
+</script>
+
+<style scoped="scoped">
+</style>
